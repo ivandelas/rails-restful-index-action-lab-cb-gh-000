ff7f7204b3f8a070185b42767e9c6f606392adf7
ivandelas <ivandelas@gmail.com>
2017-12-01 21:31:53 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,9 @@
 class Student < ActiveRecord::Base
+
+  validates :student, presence: :true,
+            :first_name, presence: :true,
+            :last_name, presence: :true
+
   def to_s
     self.first_name + " " + self.last_name
   end
